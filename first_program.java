b4929f165682c4b6af5e165d6ddba08fcb06e3f4
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-07-08 11:35:57 +0530

    print
    java

---
+++ first_program.java
@@ -0,0 +1,12 @@
+package kaleem;//reliance mall
+
+public class first_program {//super marker
+
+	public static void main(String[] args) {//buscuite section
+		// TODO Auto-generated method stub
+		System.out.println("britania 10");
+		System.out.println("milkybar 5");
+		System.out.println("gi");
+	}
+
+}
